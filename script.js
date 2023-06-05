f5fc271eb19253b59db6d845df815b4ecf0dfe69
Sebastian B <sebbarry@comcast.net>
2023-06-05 14:00:09 -0600

    First push
    Followed this guide: https://www.youtube.com/watch?v=bOd4eFqIg00

---
+++ script.js
@@ -0,0 +1,28 @@
+function signin() {
+  let oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+  let form = document.createElement("form");
+  form.setAttribute("method", "GET");
+  form.setAttribute("action", oauth2Endpoint);
+
+  let params = {
+    "client_id":"890478643384-32qojcj7o7hvlliaqgbfkri3bkgs4vto.apps.googleusercontent.com",
+    "redirect_uri":"http://127.0.0.1:5500/profile.html",
+    "response_type":"token",
+    "scope":"https://www.googleapis.com/auth/userinfo.profile",
+    "include_granted_scopes":"true",
+    "state":"pass-through-value"
+  }
+
+  for (var p in params) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "hidden");
+    input.setAttribute("name", p);
+    input.setAttribute("value", params[p]);
+    form.appendChild(input);
+  }
+
+  document.body.appendChild(form);
+
+  form.submit();
+}
