725845004bbdeda371a6dace6d71f4932c3a39a3
Sebastian B <sebbarry@comcast.net>
2023-06-07 10:31:43 -0600

    Version 3
    Updated to reflect this guide:
    https://www.youtube.com/watch?v=7_Nj7sDbBvg&list=PL3CDD0Jz5nVF0YK3Xm9Hhy4d7ui3-kSd_&index=5

--- profile.js
+++ profile.js
@@ -55,3 +55,65 @@ button.onclick = logout;
 function logout() {
   utils.logout(ACCESS_TOKEN, redirect_url);
 }
+
+let search = document.getElementById("search");
+let images = document.getElementById("images");
+let videos = document.getElementById("videos");
+let pdf = document.getElementById("pdf");
+let result = document.getElementById("result");
+
+search.onclick = listFiles;
+images.onclick = getImages;
+videos.onclick = getVideos;
+pdf.onclick = getPDF;
+
+function listFiles() {
+  searchFiles();
+}
+function getImages() {
+  searchFiles("mimeType contains 'image/'");
+}
+function getVideos() {
+  searchFiles("mimeType contains 'video/'");
+}
+function getPDF() {
+  searchFiles("mimeType contains 'application/pdf'");
+}
+ 
+function searchFiles(q = "") {
+  result.innerHTML = "";
+  fetch(
+    `https://www.googleapis.com/drive/v3/files?q=${q}&pageSize=50&supportsAllDrives=true&fields=files(name,id,mimeType)`,
+    {
+      method: "GET",
+      headers: new Headers({ Authorization: "Bearer " + ACCESS_TOKEN }),
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then(function (val) {
+      console.log(val);
+      val.files.forEach((file) => {
+        let id = file.id;
+        result.innerHTML += `
+            <tr>
+                <td><a target="_blank" href="https://drive.google.com/file/d/${file.id}">${file.name}</a>
+                </td>
+                <td>${file.mimeType}
+                <button onclick="
+                fetch('https://www.googleapis.com/drive/v3/files/${id}',{
+                    method:'DELETE',
+                    headers: new Headers({ Authorization: 'Bearer ${ACCESS_TOKEN}'})
+                })
+                .then((info) => {
+                    console.log(info)
+                    alert('file is deleted')
+                })
+                ">Delete</button>
+                </td>
+            </tr>
+            `;
+      });
+    });
+}
